8298f6cfed47df857260ef976406303a7d9d9b03
Buzil, Michael <mbuzil@iu.edu>
2024-01-21 17:48:40 -0500

    junit

---
+++ part2/src/ProblemsTest.java
@@ -0,0 +1,33 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProblemsTest {
+
+
+    @org.junit.jupiter.api.Test
+    void problem1() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void problem2() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void problem3() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void problem4() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void problem5() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void problem6() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void problem7() {
+    }
+}
